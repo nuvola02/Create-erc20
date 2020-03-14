94ec5b42bf004729a764515b87681cb43f46e511
nuvola02 <55622861+nuvola02@users.noreply.github.com>
2020-03-14 09:21:52 +0900

    Update ERC20Burnable.sol

--- ERC20Burnable.sol
+++ ERC20Burnable.sol
@@ -1,57 +1,27 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
-import "./IERC20.sol";
+import "./Context.sol";
+import "./ERC20.sol";
 
 /**
- * @dev Optional functions from the ERC20 standard.
+ * @dev Extension of {ERC20} that allows token holders to destroy both their own
+ * tokens and those that they have an allowance for, in a way that can be
+ * recognized off-chain (via event analysis).
  */
-contract ERC20Detailed is Initializable, IERC20 {
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
-
+contract ERC20Burnable is Context, ERC20 {
     /**
-     * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
-     * these values are immutable: they can only be set once during
-     * construction.
-     */
-    function initialize(string memory name, string memory symbol, uint8 decimals) public initializer {
-        _name = name;
-        _symbol = symbol;
-        _decimals = decimals;
-    }
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     * @dev Destroys `amount` tokens from the caller.
      *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
+     * See {ERC20-_burn}.
      */
-    function decimals() public view returns (uint8) {
-        return _decimals;
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
     }
 
-    uint256[50] private ______gap;
+    /**
+     * @dev See {ERC20-_burnFrom}.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        _burnFrom(account, amount);
+    }
 }
